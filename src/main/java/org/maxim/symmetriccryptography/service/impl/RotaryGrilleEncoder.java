846862f3399ee9188ee56c16c27792cb8cfb30ac
Maksim Patapenka <maxim131994@gmail.com>
2015-09-28 17:07:42 +0300

    Updated new encoder

--- src/main/java/org/maxim/symmetriccryptography/service/impl/RotaryGrilleEncoder.java
+++ src/main/java/org/maxim/symmetriccryptography/service/impl/RotaryGrilleEncoder.java
@@ -3,6 +3,30 @@ package org.maxim.symmetriccryptography.service.impl;
 import org.maxim.symmetriccryptography.service.Encoder;
 
 public class RotaryGrilleEncoder implements Encoder {
+
+    private static final boolean[][] DEFAULT_MASK = {
+            {false, true, false, false, false, false, false, false, false, false},
+            {true, false, false, false, true, false, true, true, false, false},
+            {false, true, false, false, false, true, false, false, false, true},
+            {false, false, false, true, false, false, false, true, false, false},
+            {false, true, false, false, false, false, false, false, false, false},
+            {false, false, true, false, false, true, true, false, false, true}
+    };
+
+    private boolean[][] mask;
+
+    public RotaryGrilleEncoder() {
+        this(DEFAULT_MASK);
+    }
+
+    public RotaryGrilleEncoder(boolean[][] mask) {
+        this.mask = mask;
+    }
+
+    private boolean[][] turnMask() {
+        return null;
+    }
+
     public String encrypt(String msg) {
         return null;
     }
@@ -10,4 +34,10 @@ public class RotaryGrilleEncoder implements Encoder {
     public String decrypt(String msg) {
         return null;
     }
+
+    @Override
+    public String toString() {
+        return "Rotary grille encoder";
+    }
+
 }
