91dbaea293f73ff91d7163e06809c59efc2e40a6
Maksim Patapenka <maxim131994@gmail.com>
2015-09-29 11:37:33 +0300

    Updated grille encoder

--- src/main/java/org/maxim/symmetriccryptography/service/impl/RotaryGrilleEncoder.java
+++ src/main/java/org/maxim/symmetriccryptography/service/impl/RotaryGrilleEncoder.java
@@ -1,38 +1,102 @@
 package org.maxim.symmetriccryptography.service.impl;
 
+import org.maxim.symmetriccryptography.domain.RotaryGrille;
 import org.maxim.symmetriccryptography.service.Encoder;
 
 public class RotaryGrilleEncoder implements Encoder {
 
-    private static final boolean[][] DEFAULT_MASK = {
-            {false, true, false, false, false, false, false, false, false, false},
-            {true, false, false, false, true, false, true, true, false, false},
-            {false, true, false, false, false, true, false, false, false, true},
-            {false, false, false, true, false, false, false, true, false, false},
-            {false, true, false, false, false, false, false, false, false, false},
-            {false, false, true, false, false, true, true, false, false, true}
-    };
-
-    private boolean[][] mask;
+    private RotaryGrille mask;
 
     public RotaryGrilleEncoder() {
-        this(DEFAULT_MASK);
+        this(new RotaryGrille());
     }
 
-    public RotaryGrilleEncoder(boolean[][] mask) {
+    public RotaryGrilleEncoder(RotaryGrille mask) {
         this.mask = mask;
     }
 
-    private boolean[][] turnMask() {
-        return null;
+    private int fillForPattern(char[][] bundle, RotaryGrille mask, String msg, int pos) {
+        for (int i = 0; i < mask.getRows(); i++) {
+            for (int j = 0; j < mask.getCols(); j++) {
+                if (mask.isSet(i, j)) {
+                    try {
+                        bundle[i][j] = msg.charAt(pos++);
+                    } catch (IndexOutOfBoundsException e) {
+                        return pos;
+                    }
+                }
+            }
+        }
+        return pos;
+    }
+
+    private void getForPattern(char[][] bundle, RotaryGrille mask, StringBuilder msgBuilder) {
+        for (int i = 0; i < mask.getRows(); i++) {
+            for (int j = 0; j < mask.getCols(); j++) {
+                if (mask.isSet(i, j)) {
+                    msgBuilder.append(bundle[i][j]);
+                }
+            }
+        }
     }
 
     public String encrypt(String msg) {
-        return null;
+        int maxSize = mask.getRows() * mask.getCols();
+        if (msg.length() > maxSize) {
+            throw new IllegalArgumentException("Rotary grille is too small for this message.");
+        }
+
+        char[][] tempBundle = new char[mask.getRows()][mask.getCols()];
+        int currentCharPos = 0;
+
+        currentCharPos = fillForPattern(tempBundle, mask, msg, currentCharPos);
+
+        RotaryGrille rotatedOriginGrille = RotaryGrille.rotate180(mask);
+        currentCharPos = fillForPattern(tempBundle, rotatedOriginGrille, msg, currentCharPos);
+
+        RotaryGrille flippedGrille = RotaryGrille.flipGrille(mask);
+        currentCharPos = fillForPattern(tempBundle, flippedGrille, msg, currentCharPos);
+
+        RotaryGrille rotatedFlippedGrille = RotaryGrille.rotate180(flippedGrille);
+        fillForPattern(tempBundle, rotatedFlippedGrille, msg, currentCharPos);
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < mask.getRows(); i++) {
+            for (int j = 0; j < mask.getCols(); j++) {
+                builder.append(tempBundle[i][j]);
+            }
+        }
+        return builder.toString();
     }
 
     public String decrypt(String msg) {
-        return null;
+        int maxSize = mask.getRows() * mask.getCols();
+        if (msg.length() > maxSize) {
+            throw new IllegalArgumentException("Rotary grille is too small for this message.");
+        }
+
+        char[][] tempBundle = new char[mask.getRows()][mask.getCols()];
+        int symbolPos = 0;
+        for (int i = 0; i < mask.getRows(); i++) {
+            for (int j = 0; j < mask.getCols(); j++) {
+                tempBundle[i][j] = msg.charAt(symbolPos++);
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        getForPattern(tempBundle, mask, builder);
+
+        RotaryGrille rotatedOriginGrille = RotaryGrille.rotate180(mask);
+        getForPattern(tempBundle, rotatedOriginGrille, builder);
+
+        RotaryGrille flippedGrille = RotaryGrille.flipGrille(mask);
+        getForPattern(tempBundle, flippedGrille, builder);
+
+        RotaryGrille rotatedFlippedGrille = RotaryGrille.rotate180(flippedGrille);
+        getForPattern(tempBundle, rotatedFlippedGrille, builder);
+
+        return builder.toString();
     }
 
     @Override
